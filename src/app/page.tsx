b350d6d8eae8eae83a017b4cbdb7ac6834639875
Tracey Kadenyi <treykadenyi@gmail.com>
2026-03-30 21:53:41 +0300

    add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-base text-ink">
+    <main className="relative bg-base text-ink w-full">
       <Navbar />
       <Hero />
     </main>
