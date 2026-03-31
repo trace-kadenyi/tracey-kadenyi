21743bdd29842a06e4beea70a517403e31ebad2d
Tracey Kadenyi <treykadenyi@gmail.com>
2026-03-31 14:03:21 +0300

    add projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <main className="relative bg-base text-ink w-full">
       <Navbar />
       <Hero />
+      <Projects />
     </main>
   );
 }
