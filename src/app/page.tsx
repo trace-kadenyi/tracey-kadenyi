1df748f5672d6d126f3ded7d32733c25a92e1b3f
Tracey Kadenyi <treykadenyi@gmail.com>
2026-04-01 11:36:01 +0300

    Add About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import About from "@/components/About";
 
 export default function Home() {
   return (
-    <main className="relative bg-base text-ink w-full">
+    <main className="relative bg-white text-[#0f1626] w-full">
       <Navbar />
       <Hero />
+      <About />
       <Projects />
     </main>
   );
