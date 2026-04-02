56fba56e78248df332fa51fb7149fb1c3f11e75d
Tracey Kadenyi <treykadenyi@gmail.com>
2026-04-02 12:31:32 +0300

    fix bug: fetching blogs

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,7 +1,7 @@
 export async function GET() {
   try {
     const res = await fetch(
-      "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@tracekadenyi&api_key=&order_by=pubDate",
+      "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@tracekadenyi",
       { next: { revalidate: 3600 } },
     );
     const data = await res.json();
